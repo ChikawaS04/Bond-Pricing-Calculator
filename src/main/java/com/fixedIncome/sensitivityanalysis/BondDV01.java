10e9338d625e5a99308781012d66cd38370469b7
David Davidson <ddavidson644@gmail.com>
2026-05-09 00:02:29 -0400

    Added javadocs to BondModifiedDuration and BondDV01

--- src/main/java/com/fixedIncome/sensitivityanalysis/BondDV01.java
+++ src/main/java/com/fixedIncome/sensitivityanalysis/BondDV01.java
@@ -3,10 +3,29 @@ package com.fixedIncome.sensitivityanalysis;
 import com.fixedIncome.pricer.FixedRateBondPricer;
 import com.fixedIncome.securities.Bond;
 
+/**
+ * Computes the DV01 (Dollar Value of a 01) for a fixed-rate bond.
+ *
+ * <p>DV01 measures the change in a bond's price for a 1 basis point (0.01%)
+ * decrease in yield. It is derived from modified duration:</p>
+ *
+ * <pre>
+ *   DV01 = Modified Duration × Dirty Price × 0.0001
+ * </pre>
+ */
 public class BondDV01 {
 
     BondMacaulayDuration bondMacaulayDurationSolved = new BondMacaulayDuration();
 
+    /**
+     * Calculates the DV01 for a bond at a given yield-to-maturity.
+     *
+     * @param bondModifiedDurationSolved the {@link BondModifiedDuration} instance used to compute modified duration
+     * @param fixedRateBondPricer        the pricer used to compute the bond's dirty price
+     * @param bond                       the bond for which DV01 is calculated
+     * @param ytm                        the yield-to-maturity as a decimal (e.g. 0.05 for 5%)
+     * @return the DV01 in dollars per 1 basis point move in yield
+     */
     public double calculateDV01(BondModifiedDuration bondModifiedDurationSolved, FixedRateBondPricer fixedRateBondPricer, Bond bond, double ytm) {
         double modDur = bondModifiedDurationSolved.calculateModifiedDuration(bondMacaulayDurationSolved, bond, ytm);
         return modDur * fixedRateBondPricer.dirtyPrice(bond, ytm) * 0.0001;
